b4fd05fcdbc6a495747d5646f7e1513350510ede
MohamedRamadan32325 <mr5890347@gmail.com>
2026-06-16 18:14:50 +0300

    Add ICacheService and CacheService with basic structure
    Two new files, `ICacheService.cs` and `CacheService.cs`, were added.
    `ICacheService` resides in the `Questro.Service.Abstractions.Cache` namespace,
    and `CacheService` resides in the `Questro.Service.Services.Cache` namespace.
    
    Both classes are internal and currently have no implementation.
    Standard `using` directives for .NET namespaces were included but are not yet used.

---
+++ server/Questro.Service/Services/Cache/CacheService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questro.Service.Services.Cache
+{
+    internal class CacheService
+    {
+    }
+}
